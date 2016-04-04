72c3ae5fe394f7279692f755f3007bd28bd64724
Paul <paulalexrees@gmail.com>
2016-04-04 16:03:26 +0100

    Adds rake, spaces index page. Adding space functionality working

--- app/controllers/spaces.rb
+++ app/controllers/spaces.rb
@@ -3,4 +3,14 @@ class MakersBnb < Sinatra::Base
   get '/spaces/new' do
     erb :'spaces/new'
   end
+
+  post '/spaces' do
+    Space.create(name: params[:name])
+    redirect '/spaces'
+  end
+
+  get '/spaces' do
+    @spaces = Space.all
+    erb :'spaces/index'
+  end
 end
--- app/models/data_mapper_setup.rb
+++ app/models/data_mapper_setup.rb
@@ -1,5 +1,4 @@
 require_relative 'space'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/makers_bnb_#{ENV['RACK_ENV']}")
-DataMapper.auto_migrate!
 DataMapper.finalize
