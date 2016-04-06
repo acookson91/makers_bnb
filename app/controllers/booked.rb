cc3d7e90dbf205e95538c9e32bc8f1f7b0fe1094
Interviews <interviews@Interviewss-MacBook-Pro-7.local>
2016-04-06 11:23:16 +0100

    space.update no longer changes the available date of the space. Now 'booking' has a pending column, and available date persists.

--- app/controllers/booked.rb
+++ app/controllers/booked.rb
@@ -10,16 +10,11 @@ class MakersBnb < Sinatra::Base
     space = Space.get(session[:space_id])
     user = current_user
     booking = Booking.new(booking_date: space.available_date)
-    #p "Space before booking: #{space.user}"
-    space.update(available_date: "BOOKED")
-    #p "Space after booking: #{space}"
+    # booking.update(available_date: "BOOKED")
     user.bookings << booking
     space.bookings << booking
-    #p booking
     booking.save
     user.save
-    #p user
-    #p booking
     redirect '/booked/confirm'
   end
 
