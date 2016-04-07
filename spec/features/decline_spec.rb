10513d20ddcb75d907a950309b388c08964bea5a
Claudia Green <claudiagreen@Claudias-MacBook-Pro.local>
2016-04-07 15:43:10 +0100

    Accept/Declined buttons moved. Decline status now functional. All tests passing.

--- spec/features/decline_spec.rb
+++ spec/features/decline_spec.rb
@@ -32,32 +32,9 @@ feature "Declining requests as a host:" do
       expect(page).to have_content("Space Number One")
       expect(page).to have_content "a@a.com requested this and you declined."
     end
-    # within 'a#pending-group' do
-    # expect(page).to be_empty
-    # # expect(page).to_not have_content "a@a.com requested this."
-    # end
+    expect(page).to_not have_css("a#pending-group")
+    expect(page).to_not have_css("a#approved-group")
   end
 
-  scenario "Space appears in declined column when declined 2" do
-    sign_up
-    create_space_one
-    click_button 'Log Out'
-    sign_up_again_and_again
-    request_booking
-    click_button 'Log Out'
-    click_link 'Log In'
-    sign_in
-    click_link 'My Requests'
-    click_button 'Decline'
-    visit '/spaces/myspaces'
-    # within 'a#declined-group' do
-    #   expect(page).to have_content("Space Number One")
-    #   expect(page).to have_content "a@a.com requested this and you declined."
-    # end
-    within 'a#pending-group' do
-    expect(page).to_not have_content("Space Number One")
-    expect(page).to_not have_content "a@a.com requested this."
-    end
-  end
 
 end
